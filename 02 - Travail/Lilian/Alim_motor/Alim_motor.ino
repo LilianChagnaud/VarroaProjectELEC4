b19a152781b2ba8eaa74c5ce63de31c112148a75
Lilian Chagnaud <lilchgd2003@gmail.com>
2025-11-04 16:36:00 +0100

    Code working for controling top motor with command

--- 02 - Travail/Lilian/Alim_motor/Alim_motor.ino
+++ 02 - Travail/Lilian/Alim_motor/Alim_motor.ino
@@ -1,27 +1,50 @@
 #include <AccelStepper.h>
 
-#define dirPin 12    // nouvelle broche DIR
-#define stepPin 13   // nouvelle broche STEP
+#define dirPin 12
+#define stepPin 13
 
 AccelStepper stepper(AccelStepper::DRIVER, stepPin, dirPin);
 
+// === PARAMÈTRES MECANIQUES ===
+const float stepsPerMM = 40.0; // 1 tour moteur(1600 pas) équivaut à 40mm
+const float offsetMin = 35.0; // Distance entre le bord de la planche et la limite de translation du support de caméra en mm
+const float offsetMax = 45.0;
+const float cameraSupportLength = 75.0;
+const float totalLength = 540.0; // longueur de la planche en mm
+const float usableLength = totalLength - offsetMin - offsetMax - cameraSupportLength; //383mm
+const float minPos = 0; // Position niveau moteur en mm
+const float maxPos = usableLength;
+
+// === PARAMÈTRES MOTEUR ===
+//const int stepsPerRevolution = 1600; // 1 tour moteur = 1600 pas (en 1/8 microstep)
+const int speed = 1500;              // vitesse max (pas/s)
+const int accel = 1000;               // accélération (pas/s²)
+
 void setup() {
-  stepper.setMaxSpeed(10000);
-  stepper.setAcceleration(1000);
+  Serial.begin(9600);
+  stepper.setMaxSpeed(speed);
+  stepper.setAcceleration(accel);
+  stepper.setCurrentPosition(0);
+
+  Serial.println("Prêt. Entrez une position entre 0 et 383mm :");
 }
 
 void loop() {
-  // tourner dans une direction
-  stepper.moveTo(200);
-  while(stepper.distanceToGo() != 0){
-    stepper.run();
-  }
-  delay(1000);
+  if(Serial.available() > 0){
+    float targetMM = Serial.parseFloat();
 
-  // tourner dans l'autre direction
-  stepper.moveTo(-200);
-  while (stepper.distanceToGo() != 0) {
-    stepper.run();  // Avancer vers la position cible
+    while (Serial.available() > 0) Serial.read(); // vide le buffer pour les \n et \r
+
+    if (targetMM < minPos) targetMM = minPos;
+    if (targetMM > maxPos) targetMM = maxPos;
+
+    long targetSteps = targetMM * stepsPerMM;
+    stepper.moveTo(targetSteps);
+
+    Serial.print("Deplacement vers ");
+    Serial.print(targetMM);
+    Serial.println(" mm ...");
+
   }
-  delay(1000);
+  stepper.run();
 }
